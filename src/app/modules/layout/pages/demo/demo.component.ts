17cf2dce807aa2906c8608ee7a9e90f25e8938a1
mdbasilio <mario.basilio@iroute.com.ec>
2023-08-04 16:28:10 -0500

    add fn azure

--- src/app/modules/layout/pages/demo/demo.component.ts
+++ src/app/modules/layout/pages/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { EndpointService } from 'src/app/services/endpoint.service';
@@ -12,13 +12,11 @@ export class DemoComponent {
   form: FormGroup;
   videoUrl: string = '';
   id_request: string = '';
-  @ViewChild('videoPlayer', { static: false }) videoPlayer!: ElementRef;
 
   constructor(
     private fb: FormBuilder,
     private _svcEndpoint: EndpointService,
     private spinner: NgxSpinnerService,
-    private renderer: Renderer2,
     private cd: ChangeDetectorRef) {
     this.form = this.fb.group({
       text: ['', Validators.required]
@@ -43,7 +41,7 @@ export class DemoComponent {
       this._svcEndpoint.crearVideo(body).subscribe({
         next: (resp: any) => {
           this.cd.detectChanges();
-          this.videoUrl = resp;
+          this.videoUrl = resp.result_url;
           this.spinner.hide();
         },
         error: (error: any) => {
@@ -51,41 +49,6 @@ export class DemoComponent {
           this.spinner.hide();
         }
       })
-      /* this._svcEndpoint.httpChat(body).subscribe({
-        next: (resp: any) => {
-          console.log(resp);
-          let id: string = resp;
-          this._svcEndpoint.httpObtenerHablante(id).subscribe({
-            next: (resp: any) => {
-              console.log(resp);
-              this.cd.detectChanges();
-              this.videoUrl = resp;
-              //this.loadAndPlayVideo(this.videoUrl);
-              this.spinner.hide();
-            },
-            error: (error) => {
-              console.error(error);
-              this.spinner.hide();
-            }
-          });
-        },
-        error: (err) => {
-          console.log(err);
-          this.spinner.hide();
-        }
-      }); */
     }
   }
-
-  /* loadAndPlayVideo(url: string) {
-    const videoPlayer: HTMLVideoElement = this.videoPlayer.nativeElement;
-
-    if (!videoPlayer.src) {
-      this.renderer.setAttribute(videoPlayer, 'src', url);
-      videoPlayer.load(); 
-      videoPlayer.play(); 
-    } else {
-      videoPlayer.play();
-    }
-  } */
 }
--- src/app/services/endpoint.service.ts
+++ src/app/services/endpoint.service.ts
@@ -7,10 +7,7 @@ import { RequestHablante } from '../interfaces/interfaces';
   providedIn: 'root'
 })
 export class EndpointService {
-  private url_hablante: string = '';
-  private url_botsonic: string = '';
-  private authorization: string = '';
-  private apiKey: string = '';
+  private url_fn: string = '';
 
   constructor(private http: HttpClient) {
     this.load();
@@ -19,10 +16,7 @@ export class EndpointService {
   public load() {
     this.http.get('assets/config.json').subscribe({
       next: (envResponse: any) => {
-        this.url_hablante = envResponse['url_hablante'];
-        this.authorization = envResponse['authorization'];
-        this.url_botsonic = envResponse['url_botsonic'];
-        this.apiKey = envResponse['apiKey'];
+        this.url_fn = envResponse['url_fn'];
       },
       error: (err: any) => {
         console.log(err);
@@ -31,69 +25,8 @@ export class EndpointService {
   }
 
 
-  httpChat<T>(data: any): Observable<T> {
-    const body = {
-      "question": data.text,
-      "chat_history": []
-    };
-
-    const headers = new HttpHeaders({
-      'key': 'token',
-      'token': this.apiKey
-    });
-    const endpointurl_botsonic = `${this.url_botsonic}`;
-
-    return this.http.post<T>(endpointurl_botsonic, body, { headers }).pipe(switchMap((info: any) => {
-      const answer = info[0].data.answer;
-      return this.httpCrearHablante<T>(answer);
-    }));
-  }
-
-
-  get httpOptions() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        Authorization: this.authorization
-      })
-    };
-
-    return httpOptions;
-  }
-
-  httpCrearHablante<T>(text: string): Observable<T> {
-    const data: RequestHablante = {
-      script: {
-        type: 'text',
-        provider: {
-          type: 'microsoft',
-          voice_id: 'es-EC-LuisNeural'
-        },
-        subtitles: false,
-        input: text
-      },
-      source_url: 'https://iroutestorageface.blob.core.windows.net/fotos/0725202310445778DavidDuenas.JPG',
-      driver_url: 'bank://lively/driver-06/original'
-    }
-    const endpointurl_hablante = `${this.url_hablante}`;
-    return this.http.post<T>(endpointurl_hablante, data, this.httpOptions).pipe(map((resp: any) => {
-      const id = resp.id;
-      return id;
-    }));
-  }
-
-  httpObtenerHablante<T>(id: string): Observable<T> {
-    const endpointurl_hablante = `${this.url_hablante}/${id}`;
-    return interval(900).pipe(
-      switchMap(() => this.http.get<any>(endpointurl_hablante, this.httpOptions)),
-      filter(response => !!response.result_url),
-      take(1),
-      map(response => response.result_url)
-    );
-  }
-
-
   crearVideo(data: any) {
-    const url = `http://localhost:7071/api/SpeakerFunction`;
+    const url = `${this.url_fn}`;
     return this.http.post<any>(url, data);
   }
 }
